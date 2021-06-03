246d9faab31d9849d54248798bc1dd7c9fb9571e
Nujsvart <kampfar@gmail.com>
2021-06-03 23:27:11 +0300

    returning functions, call, apply, bind methods

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -108,7 +108,133 @@ transformer("Javascript is the best", oneWord);
 //? same
 
 const high5 = function () {
-  console.log("🐕");
+  /* console.log("🐕"); */
 };
 document.body.addEventListener("click", high5); //? addEventListener -> higher-order
 ["Jonas", "Martha", "Adam"].forEach(high5); //? forEach -> higher-order
+
+console.log("✂".repeat(30));
+
+//! FUNCTIONS RETURNING FUNCTIONS
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet("Hey"); //? greet() fonk. arguman veriliyor ve return fonksiyonuna greeterHey() ismi verilip ataniyor.
+greeterHey("Esra");
+
+greet("Hello")("Esra");
+
+// challenge arrow function
+
+const greetArrow = (greeting) => (name) => console.log(`${greeting} ${name}`);
+
+/* const greeterHello = greetArrow("Hello");
+greeterHello("Esra"); */
+greetArrow("Heyyo")("Esra");
+
+console.log("✂".repeat(30));
+
+//! The CALL and APPLY METHODS
+
+const lufthansa = {
+  airline: "Lufthansa",
+  iataCode: "LH",
+  bookings: [],
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+  },
+};
+
+lufthansa.book(239, "Esra Discordia");
+
+const eurowings = {
+  airline: "Eurowings",
+  iataCode: "EW",
+  bookings: [],
+};
+
+const book = lufthansa.book; //? objedeki methodu first-class function olarak tanimladik.
+
+/* book(23, "Duman Dora"); */ //? bu sekilde calismior cunku fonksiyondaki "this" undefined oluyor. call methodu ile hangi objede kullanilacagi belirtilmesi lazim.
+
+//* Call method
+
+book.call(eurowings, 23, "Duman Dora"); //* once obje, sirasina gore arg.
+console.log(eurowings);
+
+book.call(lufthansa, 54, "Duman Delibas:)");
+console.log(lufthansa);
+
+//* Apply method
+
+//? call'dan farkli olarak, arguman yerine array aliyor. kullanisli degil
+
+const flightData = [583, "George Cooper"];
+book.apply(eurowings, flightData);
+
+//? bunun yerine call methodu kullanilip, ...spread ile arguman verilebilir.
+
+book.call(eurowings, ...flightData);
+
+console.log("✂".repeat(30));
+
+//! BIND METHOD
+
+const bookEW = book.bind(eurowings); //?eurowings'e ozel atandi.
+const bookLH = book.bind(lufthansa); //? lufthansa;ya ozel atandi.
+
+bookEW(23, "Steven Williams"); //? invoke ederken obje(this) ismine gerek kalmadi.
+
+//* flightNum parametresini ozel olarak ayarlayarak, o parametrenin degerine ozel fonksiyon atanabilir.
+
+const bookEW23 = book.bind(eurowings, 23);
+
+bookEW23("Esra Discordia"); //? simdi arguman olarak sadece isim gerekiyor.
+
+//* With Event Listeners
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  /* console.log(this); */ //! asagidaki ilk event listenerda; event handler fonksiyonlarinda "this", her zaman cagirildigi elementi isaret eder.
+  this.planes++;
+  console.log(this.planes);
+};
+
+/* document.querySelector(".buy").addEventListener("click", lufthansa.buyPlane); */
+
+document
+  .querySelector(".buy")
+  .addEventListener("click", lufthansa.buyPlane.bind(lufthansa)); //? "this"'i lufthansa objesine isaret ettik.
+
+//* Partial application
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23); //? burda "this"(obje) icin null biraktik cunku lazim degil, rate parametresini ayarlayarak yeni fonk olusturduk.
+/* const addVAT = value => value + value * 0.23; */ //? bunun aynisi
+
+console.log(addVAT(100)); //? sadece value argumani ile invoke edioruz.
+
+//* yukardaki bind methodu ile olusturulan fonksiyonu, return fonksiyon ile yapmak:
+
+const addReturn = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+console.log(addReturn(0.23)(100));
+
+/* const addRet = addReturn(0.23);
+console.log(addRet(100));
+ */
+
+console.log("✂".repeat(30));
