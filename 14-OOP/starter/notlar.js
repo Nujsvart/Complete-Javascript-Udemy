5e84dcf3139d446960d4ef80a929096eaa62c193
Nujsvart <kampfar@gmail.com>
2021-06-22 06:52:49 +0300

    Object-Oriented Programming?

---
+++ 14-OOP/starter/notlar.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//! What is Object Oriented Programming?
+
+//* Abstraction
+//? Ignoring or hiding details that don't matter, allowing us to get an overview perspective of the thing we're implementing, instead of messing with details that don't really matter to our implementation.
+//? Ornek olarak addEventListener fonksiyonun arka planda nasil calistigi detayini onemsemioruz, sadece kullanioruz.
+
+//* Encapsulation
+//? Keeping properties and methods private inside the class, so they are not accesible from outside the class. Some methods can be exposed as a public intterface (API).
+
+//* Inheritance
+//? Parent Class -> Child Class
+//? Making all properties and methods of a certain class available to a child class, forming a hierarchical relationship between classes. This allows us to reuse common logic and to model real-world relationships.
+
+//* Polymorphism
+//? A child class can overwrite a method it inherited from a parent class.
+
+
+
