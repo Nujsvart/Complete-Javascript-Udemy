7cca5127f989aa632ea66306a7e7ee8f676ad4a8
Nujsvart <kampfar@gmail.com>
2021-06-24 14:09:53 +0300

    Encapsulation

--- 14-OOP/starter/notlar.js
+++ 14-OOP/starter/notlar.js
@@ -349,23 +349,59 @@ jay.calcAge();
 //! ANOTHER CLASS EXAMPLE
 
 class Account {
+  //********************** */
+  //? 1) Public fields (instances)
+  locale = navigator.language;
+  //? 2) Private fields (instances)
+  #movements = [];
+  #pin;
+  //********************** */
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this.pin = pin;
+    //? Protected
+    this.#pin = pin;
     //? input olmadan tanimlayabiliyoruz.
-    this.movements = [];
-    this.locale = navigator.language;
+    // this._movements = [];
+    // this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
+  //? Public Interface
+  //? Public Methods
+
+  getMovements() {
+    return this.#movements;
+  }
+
   deposit(val) {
-    this.movements.push(val);
+    this.#movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
+  }
+
+  requestLoan(val) {
+    if (this.#approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+    }
+  }
+
+  //? 4) Private Methods
+  //? Chrome henuz desteklemiyor, method degil field olarak goruyor.
+
+  #approveLoan(val) {
+    return true;
+  }
+
+  //? Static method
+
+  static helper() {
+    console.log("Helper");
   }
 }
 
@@ -373,5 +409,25 @@ const acc1 = new Account("Jonas", "EUR", 1111);
 
 acc1.deposit(250);
 acc1.withdraw(140);
+acc1.requestLoan(1000);
+
+console.log(acc1.getMovements());
 
 console.log(acc1);
+
+//! ENCAPSULATION: PROTECTED PROPERTIES AND METHODS
+
+//? Korunmasini istedigimiz property ve methodlarin isminin onune underscore(_) koyuyoruz.
+
+//! ENCAPSULATION: PRIVATE CLASS FIELDS AND METHODS
+
+//? field -> property
+
+//? 1) Public fields
+//? 2) Private fields
+//? 3) Public methods
+//? 4) Private methods
+//? 5) Static
+
+// console.log(acc1.#movements) // disardan erisilemez. Error
+Account.helper();
