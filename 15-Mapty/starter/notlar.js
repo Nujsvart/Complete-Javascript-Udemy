ae1dcaf28495d23c1b0b2c99f7b31f4bc3004d22
Nujsvart <kampfar@gmail.com>
2021-06-25 17:21:48 +0300

    Using the Geolocation API

--- 15-Mapty/starter/notlar.js
+++ 15-Mapty/starter/notlar.js
@@ -1,1 +1,7 @@
 "use strict";
+
+//! Using the Geolocation API
+
+//? navigator.geolocation.getCurrentPosition(callback(konum izin verildiginde), callback(konum reddedildiginde))
+
+//? coords.latitude  -- coords.longitude (koordinatlar)
