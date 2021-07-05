0c15f120dc34e80246b320f845b1d4ea595d8288
Nujsvart <kampfar@gmail.com>
2021-07-05 22:55:49 +0300

    An Overview of Modules in JavaScript

---
+++ 17-Modern-JS-Modules-Tooling/starter/notlar.js
@@ -0,0 +1,24 @@
+"use strict";
+
+//! An Overview of Modules in JavaScript
+
+//? Module: Reusable piece of code that encapsulates implementation details.
+
+//? IMPORT (Dependency) // EXPORT (Public API)
+
+//* Why Modules?
+
+//? Compose Software: Modules are small building blocks that we put together to build complex applications
+
+//? Isolate components: Modules can be developed in isolation without thinking about the entire codebase.
+
+//? Abstract code: Implement low-level code in modules and import these abstractions into other modules.
+
+//? Organized code: Modules naturally lead to a more organized codebase.
+
+//? Reuse code: Modules allow us to easily reuse the same code, even across multiple projects.
+
+
+
+
+
